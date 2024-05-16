5eb4b887383d426292151208345bf8160545308f
John Nidarshan <jhonnidarshan@gmail.com>
2024-05-16 18:22:13 +0530

    implementations

--- FormApplication/Program.cs
+++ FormApplication/Program.cs
@@ -1,4 +1,32 @@
+using form.Data;
+using Microsoft.Azure.Cosmos;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+
+builder.Services.AddSingleton((provider) =>
+{
+    var endpointUri = configuration["CosmosDbSettings:EndpointUri"];
+    var primaryKey = configuration["CosmosDbSettings:PrimaryKey"];
+    var databaseName = configuration["CosmosDbSettings:DatabaseName"];
+
+    var cosmosClientOptions = new CosmosClientOptions
+
+    {
+        ApplicationName = databaseName
+    };
+
+    var logerFactory = LoggerFactory.Create(builder =>
+    {
+        builder.AddConsole();
+    });
+
+    var cosmosCLient = new CosmosClient(endpointUri, primaryKey, cosmosClientOptions);
+    cosmosCLient.ClientOptions.ConnectionMode = ConnectionMode.Direct;
+    return cosmosCLient;
+});
+
 
 // Add services to the container.
 
@@ -6,6 +34,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddScoped<IEmoloyeeFormRepository, EmoloyeeFormRepository>();
+builder.Services.AddScoped<IClientFormRepository, ClientFormRepository>();
+
 
 var app = builder.Build();
 
